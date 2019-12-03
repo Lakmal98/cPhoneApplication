678fd503b3aba236109af6978dfb118926f641b9
sachitha2 <shpsachitha@gmail.com>
2019-12-03 14:15:08 +0530

    sms sub menu switch case done contacts switch case done

--- main.c
+++ main.c
@@ -29,6 +29,7 @@ void contactMenu(){
     printf("\tc. Display Contacts\n");
     printf("\td. Delete Contact\n");
     printf("\te. Search Contact\n");
+    printf("\tf. Back\n");
 }
 //contact menu end
 
@@ -37,6 +38,7 @@ void smsMenu(){
     printf("\ta. Compose Message\n");
     printf("\tb. Display Message\n");
     printf("\tc. Delete Message\n");
+    printf("\td. back\n");
 }
 
 //Sub menu functions end
@@ -51,6 +53,7 @@ void switchCaseMainMenu(){
             break;
         case 2:
             smsMenu();
+            switchCaseSMSMenu();
             break;
         case 3:
             printf("Bye");
@@ -67,12 +70,55 @@ void switchCaseContactMenu(){
     scanf(" %c",&menu);
     switch(menu){
         case 'a':
-            printf("A");
+            printf("this is for add contact\n");
+            break;
+        case 'b':
+            printf("this is for update contact\n");
+            break;
+        case 'c':
+            printf("this is for display contacts\n");
+            break;
+        case 'd':
+            printf("this is for delete contact\n");
+            break;
+        case 'e':
+            printf("this is for search contact\n");
+            break;
+        case 'f':
+            printf("this is for exit\n");
+            exit(0);
             break;
         default:
-            printf("Default");
+            printf("Wrong input, Try again.\n");
+            switchCaseContactMenu();
+
     }
 
 
+}
+
+void switchCaseSMSMenu(){
+    char menu;
+    printf("Enter key for SMS menu\n");
+    scanf(" %c",&menu);
+    switch(menu){
+        case 'a':
+            printf("this is for compose sms\n");
+            break;
+        case 'b':
+            printf("this is for display smss\n");
+            break;
+        case 'c':
+            printf("this is for delete sms\n");
+            break;
+        case 'd':
+            printf("this is for exit\n");
+            exit(0);
+            break;
+        default:
+            printf("Wrong input, Try again.\n");
+            switchCaseContactMenu();
+
+    }
 }
 
