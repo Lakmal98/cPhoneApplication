a8c494881bf60093c82e31152660d2f765006bcb
SE/2017/035 <premarat_se17035@kln.ac.lk>
2019-12-03 13:28:42 +0530

    Init

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    mainMenu();
+    return 0;
+
+}
+
+
+void mainMenu(){
+    printf("1.Contacts\n");
+    printf("2.Messages\n");
+    printf("3.Exit\n");
+}
+
+
